0bd2e3bd1c854da1bd60cef184fc13a1c9da8ffa
Saul Pwanson <saul@pwanson.com>
2016-01-18 00:09:43 -0800

    uncomment logging code

--- aws/get-recent-xd.sh
+++ aws/get-recent-xd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 BUCKET=$1
 
