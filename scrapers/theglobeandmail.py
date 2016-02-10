c52e853273be8aaa6e9a8cc923cf50bd06aacb9e
Vinoth Kumar Kannan <vdraceil@gmail.com>
2016-02-10 13:41:47 +0700

    Handling the possiblity of unicode data in the clue/solution - TheGlobeAndMail Scraper

--- scrapers/theglobeandmail.py
+++ scrapers/theglobeandmail.py
@@ -61,9 +61,9 @@ class theglobeandmail(object):
         for clue_type in clue_types:
             for clue in root.xpath('//crossword/'+clue_type)[0].getchildren():
                 number = int(clue.attrib['cn'])
-                text = clue.attrib['c']
+                text = clue.attrib['c'].encode('utf-8').strip()
                 type = getattr(Constants, clue_type.upper())
-                solution = clue.attrib['a']
+                solution = clue.attrib['a'].encode('utf-8').strip()
                 crossword.add_clue(Clue(number, type, text, solution))
 
         return crossword
