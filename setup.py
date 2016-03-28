10919e93b8f4fa575bd689b953e05094759549db
Bill Booth <moronbros@gmail.com>
2016-03-28 01:22:38 -0700

    add manifest and contrib document
    add MANIFEST.in
    add CONTRIBUTING (with notes on running things)
    add tox.ini with sensible ordering/testing
    
    run ‘too’ to test everything.

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
-"""Setup module for xd"""
+"""Setup module for xdfile"""
 
 # Always prefer setuptools over distutils
 from setuptools import find_packages, setup
 # To use a consistent encoding
-from codecs import open
+# from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
+package_root = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open('README.md') as readme:
+    long_description = readme.read()
+
+with open('VERSION') as version_file:
+    print package_root
+    # import pdb;pdb.set_trace()
+    version = version_file.read().strip()
 
 setup(
     name='xd',
@@ -19,20 +24,24 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.9',
+    version=version,
 
     description='A futureproof crossword corpus toolset',
-    long_description=long_description,
+    long_description='TODO: fill this out.',
 
     # The project's main homepage.
     url='https://github.com/century-arcade/xd',
 
     # Author details
-    author='The Python Packaging Authority',
-    author_email='pypa-dev@googlegroups.com',
+    author='Saul Pwanson',
+    author_email='xd@saul.pw',
 
     # Choose your license
     license='MIT',
+
+    # Testing
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -43,29 +52,23 @@ setup(
         'Development Status :: 3 - Alpha',
 
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Intended Audience :: End Users/Desktop',
+        # 'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
 
     # What does your project relate to?
-    keywords='sample setuptools development',
+    keywords='crossword puzzles',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=find_packages(exclude=['tests']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
@@ -75,29 +78,29 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['peppercorn'],
+    install_requires=['crossword'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={
-        'sample': ['package_data.dat'],
-    },
+    # package_data={
+    #     'sample': ['package_data.dat'],
+    # },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('my_data', ['data/data_file'])],
+    # data_files=[('my_data', ['data/data_file'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
