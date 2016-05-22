1a320e7590433a35bd3bec34d990297de141609f
Saul Pwanson <saul@pwanson.com>
2016-05-21 23:44:38 -0700

    git-commit fixes

--- scripts/35-git-commit.sh
+++ scripts/35-git-commit.sh
@@ -5,10 +5,14 @@
 set -e
 set -x
 
+TODAY=`date +"%Y%m%d"`
+
 git checkout crosswords
 git checkout -b incoming_$TODAY
-git commit -a -m "incoming for $TODAY"
-git push
+git add crosswords
+git commit -m "incoming xd for $TODAY"
+git push --set-upstream origin incoming_$TODAY
+
 
 # submit pull request to gitlab (which is setup to add some people automatically on each PR)
 
