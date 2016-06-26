70c8116b7d1f42ee0e18cff176e78440f79ca68a
Andjel <andjel@gmail.com>
2016-06-26 22:51:01 +0300

    Added stats splited by day of week

--- scripts/41-pubyears.py
+++ scripts/41-pubyears.py
@@ -1,46 +1,41 @@
 #!/usr/bin/env python3
 
 import json
-from collections import Counter
+from collections import defaultdict
 
 from xdfile import utils, metadatabase as metadb
+from xdfile import year_from_date, dow_from_date
 import xdfile
 
-
-# pub/pubyears.tsv:
-"""
-   SELECT pubid,
-          year_from_date(date) AS year,
-          COUNT(*) AS num
-      FROM puzzles
-      GROUP BY pubid, year
-      WHERE year > 1900 AND year < 2100
-"""
 
 def main():
     args = utils.get_args('generate pub-years data')
 
-#    pubyears = [ (xd.publication_id(), xd.year()) for xd in xdfile.corpus() ]
-    pubyears = [ (utils.parse_pubid(r.xdid), xdfile.year_from_date(r.Date)) 
+    pubyears = [ (utils.parse_pubid(r.xdid), year_from_date(r.Date), dow_from_date(r.Date)) 
 					for r in metadb.xd_puzzles().values() ]
-
-    pubs = {}
-    for pubid, year in pubyears:
-        if pubid not in pubs:
-            pubs[pubid] = Counter()
-        try:
-            pubs[pubid][int(year)] += 1
-        except:
-            pass
+    
+    weekdays = [ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun' ] 
+    pubs = defaultdict(dict)
+    for pubid, year, dow  in pubyears:
+        if pubid not in pubs or int(year) not in pubs[pubid]:
+            pubs[pubid][int(year)] = { k:0 for k in weekdays }
+        #if int(year) not in pubs[pubid]:
+        #    pubs[pubid][int(year)] = { k:0 for k in weekdays }
+        pubs[pubid][int(year)][dow] += 1
 
     outf = utils.open_output()
 
-    for pubid, years in pubs.items():
-        for y in sorted(years.keys()):
+    for pubid, years_dow in pubs.items():
+        for y in sorted(years_dow.keys()):
             if y < 1900 or y > 2100:
                 continue
-            metadb.append_row('pub/pubyears.tsv', "pubid year num", [ pubid, y, years[y] ])
+            # Preserve weekday order
+            dow_list = []
+            for d in weekdays:
+                dow_list.append(str(years_dow[y][d]))
 
-main()
+            metadb.append_row('pub/pubyears.tsv', "pubid year total " + " ".join(weekdays), 
+                    [ pubid, y, sum(years_dow[y].values()), "\t".join(dow_list) ])
 
-
+if __name__ == "__main__":
+    main()
