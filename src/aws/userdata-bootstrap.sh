3745629de1382a296e051f8d88a4ccb7bf48df9a
Andjel <andjel@gmail.com>
2016-07-10 09:51:00 +0300

    Get config for user-bootstrap from AWS

--- src/aws/userdata-bootstrap.sh
+++ src/aws/userdata-bootstrap.sh
@@ -7,22 +7,21 @@ set -e
 # executes at the end of initialization.  These configuration parameters
 # have to be specified inline here.
 
-export REGION=us-west-2
-export BRANCH=staging_test
-export BUCKET=xd-beta.saul.pw
-export EMAIL=andjel@gmail.com
-export XD_GIT=https://github.com/andjelx/xd.git
-export GXD_GIT=git@gitlab.com:rabidrat/gxd.git
+export LC_ALL="en_US.UTF-8"
 export LOGFILE=/tmp/`date +"%Y-%m-%d"`.log
 
 exec > >(tee -i ${LOGFILE}) 2>&1
 
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update && \
-    sudo apt-get install --yes zip awscli python3-lxml python3-pip git markdown python3-boto3 && \
+    sudo apt-get install --yes language-pack-en-base zip awscli python3-lxml python3-pip git markdown python3-boto3 && \
     sudo pip3 install cssselect botocore
 
 cd $HOME
+# Get config file from AWS
+aws s3 cp --region=us-west-2 s3://xd-private/etc/config ~/config
+source ~/config
+
 echo "Clone main project repo and switch to branch ${BRANCH}"
 git clone ${XD_GIT}
 cd xd/
