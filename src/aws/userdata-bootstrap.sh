8a3a5e95707924490a47bcbee7223ea48effaaf7
Andjel <andjel@gmail.com>
2016-07-10 11:11:44 +0300

    Added to ssh ignore unknown hosts warning

--- src/aws/userdata-bootstrap.sh
+++ src/aws/userdata-bootstrap.sh
@@ -34,7 +34,8 @@ echo "Clone GXD repo"
 aws s3 cp --region=us-west-2 s3://xd-private/etc/gxd_rsa $HOME/.ssh/
 chmod 600 $HOME/.ssh/gxd_rsa
 
-cat src/aws/ssh_config >> $HOME/.ssh/config
+alias ssh="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+#cat src/aws/ssh_config >> $HOME/.ssh/config
 ssh-agent bash -c "ssh-add $HOME/.ssh/gxd_rsa; git clone ${GXD_GIT}"
 
 echo "Run deploy script"
