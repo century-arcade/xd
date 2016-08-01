74518ecab90d1068148e7d84182c882122f57cc8
Andjel <andjel@gmail.com>
2016-08-01 08:01:17 +0300

    Fix wwwroot path reference

--- scripts/30-mkwww.sh
+++ scripts/30-mkwww.sh
@@ -13,22 +13,22 @@ cp $GXD/*.tsv $WWW/pub/gxd/
 cp $PUB/*.tsv $WWW/pub/
 
 echo -en "${GREEN}Generate /pub/[<pub>][<year>]${NORMAL}\n"
-scripts/31-mkwww-publishers.py $CORPUS -o $WWW
+scripts/31-mkwww-publishers.py $CORPUS -o $WWW/
 
 echo -en "${GREEN}Generate /pub/word/<ANSWER>${NORMAL}\n"
-scripts/33-mkwww-words.py $CORPUS -o $WWW
+scripts/33-mkwww-words.py $CORPUS -o $WWW/
 
 echo -en "${GREEN}Generate /pub/clue/<boiledclue>${NORMAL}\n"
-scripts/34-mkwww-clues.py $CORPUS -o $WWW
+scripts/34-mkwww-clues.py $CORPUS -o $WWW/
 
 echo -en "${GREEN}Generate /pub/<xdid>${NORMAL}\n"
-scripts/35-mkwww-diffs.py $CORPUS -o $WWW
+scripts/35-mkwww-diffs.py $CORPUS -o $WWW/
 
 echo -en "${GREEN}Generate /pub/clue/<xdid>${NORMAL}\n"
-scripts/36-mkwww-deepclues.py $CORPUS -o $WWW
+scripts/36-mkwww-deepclues.py $CORPUS -o $WWW/
 
 echo -en "${GREEN}From gxd/redirects.tsv${NORMAL}\n"
-scripts/38-mkwww-redirects.py -o $WWW $GXD/redirects.tsv
+scripts/38-mkwww-redirects.py -o $WWW/ $GXD/redirects.tsv
 
 # Reset color
 tput sgr0
