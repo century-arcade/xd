8482a89e420e5c815808c052fe1f8295425f56df
Saul Pwanson <xd@saul.pw>
2016-08-19 00:17:10 -0700

    pubyears start

--- scripts/22-pubyears.py
+++ scripts/22-pubyears.py
@@ -12,9 +12,21 @@ def main():
     args = utils.get_args('generate pub-years data')
 
     pubyears = [ (utils.parse_pubid(r.xdid), year_from_date(r.Date), dow_from_date(r.Date)) 
-					for r in metadb.xd_puzzles().values() ]
-    
-    weekdays = [ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun' ] 
+                    for r in metadb.xd_puzzles().values() ]
+
+    weekdays = [ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun' ]
+
+    metasql.execute("INSERT INTO stats VALUES (?,?,?, ?,?, ?, ?,?,?, ?,?, ?,?)",
+            (pubid, year, weekday,
+                maineditor, maincopyright,
+                nexisting, nxd, npublic,
+                reprints, touchups, redones,
+                copies, themecopies))
+
+
+
+
+
     pubs = defaultdict(dict)
     for pubid, year, dow  in pubyears:
         if pubid not in pubs or int(year) not in pubs[pubid]:
@@ -29,12 +41,13 @@ def main():
             if y < 1900 or y > 2100:
                 continue
             # Preserve weekday order
-            dow_list = []
             for d in weekdays:
-                dow_list.append(str(years_dow[y][d]))
-
-            metadb.append_row('pub/pubyears.tsv', "pubid year total " + " ".join(weekdays), 
-                    [ pubid, y, sum(years_dow[y].values()), "\t".join(dow_list) ])
+                ndup = 0
+                nsusp = 0
+                for r in metasql.xd_similar():
+                    "SELECT * FROM similar WHERE pubid = ? AND year = ?", pubid, y)
+                ("INSERT INTO pubyears VALUES (?, ?, ?, ?, ?)metadb.append_row('pub/pubyears.tsv', "pubid year weekday total ndup nsusp",
+                    [ pubid, y, d, years_dow[y][d], ndup, nsusp])
 
 if __name__ == "__main__":
     main()
