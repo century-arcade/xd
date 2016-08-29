5520dd0c7398d99317efdf9b667f11e13beccdb7
Saul <andjel@gmail.com>
2016-08-29 15:33:40 +0300

    Added intermediate s3 sync

--- scripts/30-mkwww.sh
+++ scripts/30-mkwww.sh
@@ -14,24 +14,31 @@ cp $PUB/*.tsv $WWW/pub/
 
 echo -en "${GREEN}Generate /pub/[<pub>][<year>]${NORMAL}\n"
 scripts/31-mkwww-publishers.py $CORPUS -o $WWW/
+aws s3 mv --recursive --region $REGION $WWW ${S3WWW}/ --acl public-read
 
 echo -en "${GREEN}Generate /pub/ index${NORMAL}\n"
 scripts/37-pubyear-svg.py -o $WWW/
+aws s3 mv --recursive --region $REGION $WWW ${S3WWW}/ --acl public-read
 
 echo -en "${GREEN}Generate /pub/word/<ANSWER>${NORMAL}\n"
 scripts/33-mkwww-words.py $CORPUS -o $WWW/
+aws s3 mv --recursive --region $REGION $WWW ${S3WWW}/ --acl public-read
 
 echo -en "${GREEN}Generate /pub/clue/<boiledclue>${NORMAL}\n"
 scripts/34-mkwww-clues.py $CORPUS -o $WWW/
+aws s3 mv --recursive --region $REGION $WWW ${S3WWW}/ --acl public-read
 
 echo -en "${GREEN}Generate /pub/<xdid>${NORMAL}\n"
 scripts/35-mkwww-diffs.py $CORPUS -o $WWW/
+aws s3 mv --recursive --region $REGION $WWW ${S3WWW}/ --acl public-read
 
 echo -en "${GREEN}Generate /pub/clue/<xdid>${NORMAL}\n"
 scripts/36-mkwww-deepclues.py $CORPUS -o $WWW/
+aws s3 mv --recursive --region $REGION $WWW ${S3WWW}/ --acl public-read
 
 echo -en "${GREEN}From gxd/redirects.tsv${NORMAL}\n"
 scripts/38-mkwww-redirects.py -o $WWW/ $GXD/redirects.tsv
+aws s3 mv --recursive --region $REGION $WWW ${S3WWW}/ --acl public-read
 
 # Reset color
 tput sgr0
