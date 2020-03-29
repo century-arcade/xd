e117c83666ad24c50136179e669794654d89fe57
jmviz <jmviz@users.noreply.github.com>
2020-03-29 14:25:38 -0400

    fix xwi parsing

--- xdfile/xwordinfo2xd.py
+++ xdfile/xwordinfo2xd.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# import string
-# import os.path
 import re
-
-from lxml import html
+from lxml import html, etree
+from datetime import datetime
 from xdfile.utils import info, debug, error
 import xdfile
 
 SPLIT_REBUS_TITLES = "CRYPTOCROSSWORD TIC-TAC-TOE".split()
 
-
-def stringify_children(node):
-    s = node.text
-    if s is None:
-        s = ''
-    # for child in node:
-    #     s += etree.tostring(child, encoding='unicode')
-    return s
-
+class XWordInfoParseError(Exception):
+    pass
 
 # content is unicode()
 def parse_xwordinfo(content, filename):
@@ -81,29 +72,34 @@ def parse_xwordinfo(content, filename):
     xd = xdfile.xdfile('', filename)
 
     # get crossword info
-    title = root.cssselect(xwiprefix + 'TitleLabel')[0].text.strip()
+    title = root.cssselect('#PuzTitle')[0].text.strip()
     try:
-        subtitle = root.cssselect(xwiprefix + 'SubTitleLabel')[0].text.strip()
+        subtitle = root.cssselect(xwiprefix + 'SubTitle')[0].text.strip()
         subtitle = ' [%s]' % subtitle
     except:
         subtitle = ""
 
-    # author = root.cssselect(xwiprefix + 'AuthorLabel')[0].text.strip()
-    # editor = root.cssselect(xwiprefix + 'EditorLabel')[0].text.strip()
-    try:
-        xd.notes = stringify_children(root.cssselect(xwiprefix + 'NotepadDiv')[0])
-    except Exception as e:
-        xd.notes = ""
-        debug('Exception %s' % e)
+    author = root.cssselect('.aegrid div')[1].text.strip()
+    editor = root.cssselect('.aegrid div')[3].text.strip()
+    
+    copyright = root.cssselect(xwiprefix + 'Copyright')[0].text.strip()
 
     xd.set_header("Title", '%s%s' % (title, subtitle))
-    xd.set_header("Author", root.cssselect(xwiprefix + 'AuthorLabel')[0].text.strip())
-    xd.set_header("Editor", root.cssselect(xwiprefix + 'EditorLabel')[0].text.strip())
+    xd.set_header("Author", author)
+    xd.set_header("Editor", editor)
+    xd.set_header("Copyright", copyright)
 
-    xd.notes = xd.notes.replace("<br/>", "\n")
-    xd.notes = xd.notes.replace("<b>Notepad:</b>", "\n")
-    xd.notes = xd.notes.replace("&#13;", "\n")
-    xd.notes = xd.notes.strip()
+    # nyt title normally has date as e.g. January 1, 2020
+    date_re = "(January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2},\s+\d{4}"
+    try:
+        m = re.search(date_re, subtitle if subtitle else title)
+        date_string = m.group(0)
+        date = datetime.strptime(date_string, "%B %d, %Y")
+        xd.set_header("Date", date.strftime("%Y-%m-%d"))
+    except:
+        pass
+
+    _process_notes(xd, xwiprefix, root) # add header for notes, if any
 
     puzzle_table = root.cssselect(xwiprefix + 'PuzTable tr') or root.cssselect('#PuzTable tr')
 
@@ -113,7 +109,7 @@ def parse_xwordinfo(content, filename):
             # check if the cell is special - with a shade or a circle
             cell_class = cell.get('class')
             cell_type = ''
-            if cell_class == 'bigshade':
+            if cell_class == 'shade':
                 cell_type = 'shaded'
             elif cell_class == 'bigcircle':
                 cell_type = 'circle'
@@ -132,8 +128,13 @@ def parse_xwordinfo(content, filename):
                             subst = "/".join(list(subst[0].text))
                         else:
                             subst = subst[0].text
-                    else:
-                        subst = ''
+                    else: # check if color rebus
+                        cell_string = etree.tostring(cell).decode('utf-8')
+                        m = re.search("background-color:([A-Z]+);", cell_string)
+                        if m:
+                            subst = m.group(1)
+                        else:
+                            subst = ''
 
                 if subst:
                     if subst not in rebus:
@@ -165,52 +166,91 @@ def parse_xwordinfo(content, filename):
     if special_type:
         xd.set_header("Special", special_type)
 
-    # add clues
-    across_clues = _fetch_clues(xd, 'A', root, xwiprefix + 'AcrossClues', rebus)
-    down_clues = _fetch_clues(xd, 'D', root, xwiprefix + 'DownClues', rebus)
+    across_div = root.cssselect('#ACluesPan') or root.cssselect(xwiprefix + 'ACluesPan')
+    down_div = root.cssselect('#DCluesPan') or root.cssselect(xwiprefix + 'DCluesPan')
+
+    if across_div and down_div: # normal puzzle
+        _process_clues(xd, 'A', across_div) # add across clues
+        _process_clues(xd, 'D', down_div) # add down clues
+    elif across_div: # uniclue puzzle?
+        _process_uniclues(xd, across_div)
+    else: 
+        raise XWordInfoParseError("No clue divs found.")
 
     return xd
 
+def _process_notes(xd, xwiprefix, root):
+    note_div = root.cssselect('#notepad') or root.cssselect(xwiprefix + 'NotepadDiv')
+    if note_div:
+        note_div_string = etree.tostring(note_div[0]).decode('utf-8')
+        note_div_string = note_div_string.replace("<br/>", "\n")
+        note_div_string = note_div_string.replace("{*Notepad:*}", "\n")
+        note_div_string = note_div_string.replace("&#13;", "\n")
+        note_div_string = note_div_string.strip()
+        note_div = html.fromstring(note_div_string)
+        note_text = note_div.text_content()
+        note_text = note_text.replace("\n", "   ")
+        note_text = note_text.strip()
+        xd.set_header("Notes", note_text)
+    elif root.cssselect(xwiprefix + 'UnicluePan'):
+        note_text = ("This was published as a uniclue puzzle in print. " +
+        "All the clues appear in a single list, combining Across and Down. " + 
+        "When two answers share a number, they also share a clue.")
+        xd.set_header("Notes", note_text) 
 
-def _fetch_clues(xd, clueprefix, root, css_identifier, rebus):
-    PT_CLUE = re.compile(r'(\d+)\. ?(.*)')
-    text = number = solution = None
-    answer_next = False
-    cluespan = root.cssselect(css_identifier)
-    if not cluespan:
-        return
-    for content in cluespan[0].itertext():
-        content = content.strip()
-        if answer_next:
-            assert not solution
-            solution = content
-
-            # replace rebuses with appropriate identifiers (numbers)
-            for item in rebus:
-                if item in content:
-                    pass
-                    # TODO: where is 'index'???
-                    # content = content.replace(item, str(index + 1))
-
-            solution = content
-            xd.clues.append(((clueprefix, number), text, solution))
-            text = number = solution = None
-            answer_next = False
+def _process_clues(xd, clueprefix, clues_div):
+    error_text = 'Parsing %s clues failed. ' % ('Across' if clueprefix == 'A' else 'Down')
+    numclue_divs = clues_div[0].cssselect('.numclue div')
+    if len(numclue_divs) % 2 != 0:
+        raise XWordInfoParseError(error_text + 
+            'Either the number of numbers does not match the ' +
+            'number of clues, or there are additional unknown divs.')
+    for i in range(0, len(numclue_divs), 2):
+        num = numclue_divs[i].text
+        clue_div = numclue_divs[i + 1]
+        clue_end = clue_div.text.rfind(' :')
+        if clue_end < 0:
+            raise XWordInfoParseError(error_text + 
+                'Malformed clue for number %s.' % num)
+        clue = clue_div.text[:clue_end]
+        anchor = clue_div.cssselect('a')
+        if len(anchor) != 1:
+            raise XWordInfoParseError(error_text + 
+                'Not exactly one anchor in clue div for number %s.' % num)
         else:
-            if content[-2:] == " :":
-                answer_next = True
-                content = content[:-2]
+            answer = anchor[0].text
+            xd.clues.append(((clueprefix, num), clue, answer))
 
-            match = re.match(PT_CLUE, content)
-            if match:
-                number = int(match.group(1))
-                text = match.group(2)
+def _process_uniclues(xd, clues_div):
+    error_text = 'Parsing suspected uniclues failed. '
+    grid_answers = xd.iteranswers()
+    down_clues = []
+    numclue_divs = clues_div[0].cssselect('.numclue div')
+    if len(numclue_divs) % 2 != 0:
+        raise XWordInfoParseError(error_text + 
+            'Either the number of numbers does not match the ' +
+            'number of clues, or there are additional unknown divs.')
+    for i in range(0, len(numclue_divs), 2):
+        num = numclue_divs[i].text
+        clue_div = numclue_divs[i + 1]
+        clue_end = clue_div.text.rfind(' :')
+        if clue_end < 0:
+            raise XWordInfoParseError(error_text + 
+                'Malformed clue for number %s.' % num)
+        clue = clue_div.text[:clue_end]
+        anchor = clue_div.cssselect('a')
+        if not anchor or len(anchor) > 2:
+            raise XWordInfoParseError(error_text + 
+                'Neither 1 nor 2 anchors in clue div for number %s.' % num)
+        for a in anchor:
+            answer = a.text
+            direction, grid_num, _ = next(grid_answers)
+            if direction == 'A':
+                xd.clues.append(((direction, grid_num), clue, answer))
             else:
-                if text is None:  # a special one
-                    number = content
-                    text = ""
-                else:
-                    text += " " + content
+                down_clues.append(((direction, grid_num), clue, answer))
+    for clue in down_clues:
+        xd.clues.append(clue)
 
 if __name__ == "__main__":
     import sys
